7fdd79a5f1c884a610bf7283de74ad1ddf9e3311
Guse, Richard <Richard.Guse@cdk.com>
2019-07-11 10:35:57 -0700

    update

--- scanNonPrintable.pl
+++ scanNonPrintable.pl
@@ -1,5 +1,6 @@
 use strict;
 use File::Spec;
+$| = 1;
 doIt(".");
 exit;
 sub doIt {
@@ -18,23 +19,32 @@ sub doIt {
             doIt($spec);
         }
         else {
+            if($content !~ /\.js$/) {
+                next;
+            }
+            #if(-B "$spec") {
+            #    #print "$spec (BINARY)\n";
+            #    next;
+            #}
             open(IFP, "<$spec");
             my $lines = do { local $/; <IFP> };
             close IFP;
-            my $cc = 0;
-            my $lc = 0;
+            my $cc = -1;
+            my $lc = 1;
             foreach my $c (split //, $lines) {
+                $cc++;
                 my $o = ord($c);
                 if(($o >= 32 && $o <= 126) ||
                     $o == 9) {
                     next;
                 }
                 if($o == 10) {
+                    $cc = -1;
                     $lc++;
                     next;
                 }
-                print "$spec (line $lc, char $cc) (char='$c', ord='$o')\n";
-                $cc++;
+                print "$spec (line #$lc, line char #$cc, ord='$o')\n";
+                last;
             }
         }
     }
